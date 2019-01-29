6fcf70189aafae6eb2cd1460e675e62cf6d98e62
yeray <yeray.alvarez.romero@gmail.com>
2019-01-29 16:24:57 +0000

    WIP

--- mergesort.py
+++ mergesort.py
@@ -1,27 +1,40 @@
-import pytest
+def mergeSort(alist):
+    print("Splitting ", alist)
+    if len(alist) > 1:
+        import ipdb
+
+        ipdb.set_trace()
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i = i + 1
+            else:
+                alist[k] = righthalf[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
+    print("Merging ", alist)
 
 
-def mergesort(values):
-    if not len(values) or len(values) == 1:
-        return values
-    if len(values) == 2:
-        if values[0] > values[1]:
-            values[0], values[1] = values[1], values[0]
-        return values
-    else:
-        mergesort(values[0 : len(values) // 2])
-        mergesort(values[len(values) // 2 + 1 :])
-        merge()
-    return values
-
-
-def merge(array_1, array_2):
-    return []
-
-
-@pytest.mark.parametrize(
-    "values,expected",
-    [([], []), ([1], [1]), ([2, 1], [1, 2]), ([2, 1, 4, 3, 5], [1, 2, 3, 4, 5])],
-)
-def tests(values, expected):
-    assert mergesort(values) == expected
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+mergeSort(alist)
+print(alist)
